efc84d630b2b4749bfa47f8f09481dd343adf4d7
unknown <kamraneze@code.edu.az>
2022-11-18 23:10:57 +0400

    HomeWork: ToUpper,ToLower, Classes

---
+++ Home work StringBuilder, Class/Program.cs
@@ -0,0 +1,42 @@
+﻿#region Cumlenin her sozu boyuk herfle baslasin.
+//Console.WriteLine("Enter a sentence:");
+//string sentence = Console.ReadLine();
+//string sentence2 = sentence.ToUpper();
+
+//Console.Write("Result: ");
+//Sentencee(sentence2);
+
+//static void Sentencee(string sentence)
+//{
+//    for (int i = 0; i < sentence.Length; i++)
+//    {
+//        if (i == 0)
+//        {
+//            Console.Write(sentence[i]);
+//            sentence = sentence.ToLower();
+//            i++;
+//        }
+//        if (sentence[i] == ' ')
+//        {
+//            sentence = sentence.ToUpper();
+//            Console.Write(sentence[i]);
+//            i++;
+//        }
+
+//        Console.Write(sentence[i]);
+//        sentence = sentence.ToLower();
+//    }
+//}
+#endregion
+
+
+#region Student Class
+//using Home_work_StringBuilder__Class;
+
+//Student student = new Student("Kamran", " Zeynalov");
+
+//Student student1 = new Student("Kamran", "Zeynalov", 2, 90, true);
+//student1.Graduated();
+//student1.Points(); 
+#endregion
+
---
+++ Home work StringBuilder, Class/Student.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_work_StringBuilder__Class
+{
+    internal class Student
+    {
+        public string Name;
+        public string SurName;
+        public byte Group;
+        public int Point;
+        public bool IsGraduated;
+
+
+        public Student(string name, string surname)
+        {
+            Name = name;
+            SurName = surname;
+            //Console.WriteLine("Name and Surname: " +Name + SurName);
+
+        }
+
+        public Student(string name, string surname, byte group, byte point, bool isgraduated)
+        {
+            Name = name;
+            SurName = surname;
+            Group = group;
+            Point = point;
+            IsGraduated = isgraduated;
+
+            Console.WriteLine("Name: " + Name + " Surname: " + SurName + " Group: " + Group + " Point: " + Point + " Is Graduated:");
+
+        }
+
+        public void Graduated()
+        {
+            if (IsGraduated)
+            {
+                Console.WriteLine("This student has graduated");
+            }
+            else
+            {
+                Console.WriteLine("This student hasn't graduated");
+
+            }
+        }
+        public void Points()
+        {
+            if (Point > 80)
+            {
+                Console.WriteLine("Congratulations:) See you next exam");
+            }
+            else
+            {
+                Console.WriteLine("Unlucky:( You will come next year.");
+
+            }
+        }
+        //public void NameAndSurname()
+        //{ 
+        
+        //}
+
+    }
+ }
+
